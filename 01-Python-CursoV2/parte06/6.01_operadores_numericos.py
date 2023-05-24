1b2c9a57219670dbb4072cc3229cbc4febd28daa
Abraham Yance <122916029+IsYancem@users.noreply.github.com>
2023-05-23 19:06:57 -0500

    Operator +

--- 01-Python-CursoV2/parte06/6.01_operadores_numericos.py
+++ 01-Python-CursoV2/parte06/6.01_operadores_numericos.py
@@ -11,10 +11,35 @@ print(f'La suma de {numero_1} y {numero_2} es igual a {suma}')
 
 print()
 
-sum = 2 + 9
+suma = 2 + 9
 print(f'La suma de {numero_1} y {numero_2} es igual a {suma}')
 
 print()
 
-sum = numero_1 + 9
+suma = numero_1 + 9
 print(f'La suma de {numero_1} y {numero_2} es igual a {suma}')
+
+print()
+
+# Suma de n cantidad de elementos:
+# Se pueden tener n cantidad de elementos a sumar
+print('Se pueden tener n cantidad de elementos a sumar:')
+suma = 1 + 2 + 3 + 4 + 5
+print(f'La suma es igual a {suma}')
+
+print()
+
+# Suma de numeros reales (punto flotante):
+print('Suma de numeros reales (punto flotante):')
+numero_3 = 3.5896
+suma =  numero_3 + 2.023 + 3.66 + 4.15 + 51.6
+print(f'La suma es igual a {suma}')
+
+numero_3 = 3.5896
+suma =  numero_3 + numero_2 + numero_1
+print(f'La suma es igual a {suma}')
+
+suma =  7.81 + 2.023 + 3.66 + 4.15 + 51.6
+print(f'La suma es igual a {suma}')
+
+print()
