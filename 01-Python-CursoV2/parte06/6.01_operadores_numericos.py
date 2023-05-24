379923ddae138817c4f7e63dc7b6334dbfe4729a
Abraham Yance <122916029+IsYancem@users.noreply.github.com>
2023-05-23 19:01:45 -0500

    Chapter 06

---
+++ 01-Python-CursoV2/parte06/6.01_operadores_numericos.py
@@ -0,0 +1,20 @@
+# Operadores numericos
+
+# Operador suma +
+
+numero_1 = 2
+numero_2 = 9
+
+suma = numero_1 + numero_2
+
+print(f'La suma de {numero_1} y {numero_2} es igual a {suma}')
+
+print()
+
+sum = 2 + 9
+print(f'La suma de {numero_1} y {numero_2} es igual a {suma}')
+
+print()
+
+sum = numero_1 + 9
+print(f'La suma de {numero_1} y {numero_2} es igual a {suma}')
