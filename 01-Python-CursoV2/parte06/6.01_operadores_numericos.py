8e40359a3d10227c8e113482aa98f2bbf66e3713
Abraham Yance <122916029+IsYancem@users.noreply.github.com>
2023-05-23 19:15:58 -0500

    Operator -

--- 01-Python-CursoV2/parte06/6.01_operadores_numericos.py
+++ 01-Python-CursoV2/parte06/6.01_operadores_numericos.py
@@ -1,3 +1,5 @@
+import math
+
 # Operadores numericos
 
 # Operador suma +
@@ -43,3 +45,61 @@ suma =  7.81 + 2.023 + 3.66 + 4.15 + 51.6
 print(f'La suma es igual a {suma}')
 
 print()
+
+# OPERADOR RESTA:
+print('OPERADOR RESTA:')
+
+numero_1 = 5
+numero_2 = 2
+
+resta = numero_1 - numero_2 # 5 - 2
+print(f'La resta de {numero_1} y {numero_2} es igual a: {resta}')
+
+print()
+
+resta = numero_1 - 3
+print(f'La resta de {numero_1} y {3} es igual a: {resta}')
+
+print()
+
+print('Expresion de resta de multiplos valores:')
+resta = 5 - 6 - 1 - 4 - 12
+print(f'La resta es igual a: {resta}')
+
+print()
+
+# Resta de valores numericos reales (punto flotante):
+print('Resta de valores numericos reales (punto flotante):')
+
+resta = 5.21 - 2.325 - 1.02
+print(f'La resta es igual a: {resta}')
+
+numero_1 = 598.25
+numero_2 = 352.236
+resta = numero_1 - numero_2 - 15
+print(f'La resta es igual a: {resta}')
+
+print()
+
+# Invertir el signo de una literal numerica (entera o real) con el operador de resta (-):
+print('Invertir el signo de una literal numerica (entera o real) con el operador de resta (-):')
+
+numero = 100
+
+print('El valor de variable numero es igual a:', numero)
+print('El valor de variable numero es igual a:', -numero)
+print('El valor de variable numero es igual a:', numero)
+
+print()
+
+numero = -100
+
+print('El valor de variable numero es igual a:', numero)
+print('El valor de variable numero es igual a:', -numero)
+print('El valor de variable numero es igual a:', numero)
+
+print()
+
+pi = math.pi
+print('El valor de la variable pi es igual a', pi)
+print('El valor de la variable pi es igual a', -pi)
