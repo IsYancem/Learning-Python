d80c3da33c44c50fdc540f00f39a7f293911975a
Abraham Yance <122916029+IsYancem@users.noreply.github.com>
2023-05-27 11:42:56 -0500

    parte09

---
+++ 01-Python-CursoV2/parte09/ex9.02_invertir_frase.py
@@ -0,0 +1,23 @@
+# Ejercicio 9.2: Capturar una palabra o frase del usuario e invertirla.
+
+frase = input('Escriba una frase o palabra: ')
+
+print('La frase original es:', frase)
+
+# Python => nohtyP
+
+# Solución 1:
+
+frase_invertada = ''
+
+for i in range(len(frase) - 1, -1, -1):
+    frase_invertada += frase[i]
+
+print('Frase invertida:', frase_invertada)
+
+print()
+
+# Solución 2:
+
+frase_invertada = frase[::-1]
+print('Frase invertida:', frase_invertada)
