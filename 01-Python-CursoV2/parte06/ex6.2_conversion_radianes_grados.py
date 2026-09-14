07b638d9b8370e69cdaba4e68c37cf916c64460e
Abraham Yance <122916029+IsYancem@users.noreply.github.com>
2026-09-14 16:15:49 -0500

    Remove multiple exercises and example scripts related to functions, exceptions, and recursion from the Python course repository.

--- 01-Python-CursoV2/parte06/ex6.2_conversion_radianes_grados.py
+++
@@ -1,8 +0,0 @@
-# Ejercicio 2. Convertir radianes a grados.
-import math
-
-radianes = float(input('Inserte el numero de radianes:'))
-
-grados = (180 * radianes)/math.pi
-
-print('El numero de grados es: {}'.format(grados))
